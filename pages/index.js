9d4e0046d0df8959f195066c3f7b144a5bdd07bb
Vladimir Lunev <lunvvova42@gmail.com>
2020-05-03 14:32:10 +0300

    Fixed core fonts Fixed color palette

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Typography } from 'antd';
+
+const { Text } = Typography;
 
 export default function Home() {
   return (
     <div>
       asdasd
+      <Text className="text-primary">adasdsd</Text>
     </div>
   );
 };
