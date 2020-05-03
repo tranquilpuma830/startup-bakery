bacef458b8b6c5c8ba4ccd3d1a28818ece669cc8
DanilR <aringai09@gmail.com>
2020-05-03 23:58:47 +0300

    Some fixes

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,64 @@
 import React from 'react';
 import Link from 'next/link';
-import { Button, Typography, Layout } from 'antd';
+import { Input, Typography, Layout, Form } from 'antd';
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
 export default function Home() {
   return (
-    <Layout className='cover-container d-flex h-100 p-3 mx-auto flex-column bg-half'>
+    <Layout className='cover-container d-flex h-100 mx-auto flex-column bg-white bg-half-filled'>
       <header class='masthead mb-auto'>
-        <div class='inner'>
+        <div class='inner bg-white p-3'>
           <nav class='d-flex nav nav-masthead justify-content-between align-items-center font-weight-bold'>
             <a class='navbar-brand' href='#'>
               <img src='/logo.svg' width='60' height='60' class='d-inline-block' alt='Startup bakery' />
               StartupBakery
             </a>
             <div className='d-flex w-auto float-right text-secondary'>
-              <a class='nav-link' href='#'>
+              <a class='text-secondary nav-link' href='#'>
                 Курсы
               </a>
-              <a class='nav-link' href='#'>
+              <a class='text-secondary nav-link' href='#'>
                 Команды
               </a>
-              <a class='nav-link' href='#'>
+              <a class='text-secondary nav-link' href='#'>
                 Новости
               </a>
-              <a class='nav-link' href='#'>
+              <a class='text-secondary nav-link' href='#'>
                 Вакансии
               </a>
             </div>
           </nav>
         </div>
       </header>
-      <Content className='d-flex inner cover align-items-center'>
-        <div className='my-auto'>
-          <Title>Title</Title>
-          <Link href='#'>
-            <a className='btn btn-outline-primary'>Дальше</a>
-          </Link>
+      <Content className='d-flex p-4 ml-2 align-items-center bg-guy'>
+        <div className='my-auto h-100'>
+          <Form {...layout} name='basic' initialValues={{ remember: true }} className='my-auto'>
+            <Title>Войти</Title>
+
+            <Form.Item name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
+              <Input placeholder='Email' />
+            </Form.Item>
+
+            <Form.Item name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
+              <Input.Password placeholder='Пароль' />
+            </Form.Item>
+
+            <Form.Item>
+              <Link href='ideas'>
+                <a className='btn btn-outline-primary'>Дальше</a>
+              </Link>
+            </Form.Item>
+          </Form>
         </div>
       </Content>
     </Layout>
