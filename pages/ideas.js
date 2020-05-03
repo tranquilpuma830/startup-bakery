7a3308791c57b9ef4c984169931f911bf2f222e7
DanilR <aringai09@gmail.com>
2020-05-03 18:53:01 +0300

    Remove <Page> wrapper from _app NOTE: You need to wrap yor page with <Page> component by yourself

--- pages/ideas.js
+++ pages/ideas.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Button } from "antd";
+import Page from '../containers/Page';
+
 
 export default function Ideas() {
   return (
-    <div>
-      asdasd
-      <Button color="primary">asdasd</Button>
-    </div>
+    <Page>
+      <div>
+        asdasd
+        <Button color='primary'>asdasd</Button>
+      </div>
+    </Page>
   );
 };
