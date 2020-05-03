8f9f6d56234d69d9a77fa37836651af56a5afbca
Vladimir Lunev <lunvvova42@gmail.com>
2020-05-03 13:49:31 +0300

    Customize Antd styles Added base page structure

--- pages/ideas.js
+++ pages/ideas.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from "antd";
 
 export default function Ideas() {
   return (
     <div>
       asdasd
+      <Button color="primary">asdasd</Button>
     </div>
   );
 };
