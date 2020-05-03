916e9e3dca24b6e7915d413f4f39a4a0bce43725
Vladimir Lunev <lunvvova42@gmail.com>
2020-05-03 13:02:23 +0300

    Added Page wrapper for pages Change page icon Added variables for bootstrap config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/main.scss';
 
+import React from "react";
+
+import Page from "../containers/Page";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
--- pages/ideas.js
+++ pages/ideas.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Layout } from 'antd';
 
-export default function Home() {
+export default function Ideas() {
   return (
     <div>
       asdasd
