7c3d0362233b2a32e1997264d89a332b2ed09a31
Vladimir Lunev <lunvvova42@gmail.com>
2020-05-04 20:38:30 +0300

    Aded integration with Firebase via firebase SDK

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
+const path = require('path');
 const withSass = require("@zeit/next-sass");
 const withLess = require("@zeit/next-less");
+const Dotenv = require('dotenv-webpack');
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== "undefined") {
@@ -11,6 +15,22 @@ module.exports = withSass(
     ignoreOrder: true,
     lessLoaderOptions: {
       javascriptEnabled: true,
+    },
+    exportPathMap: () => ({}), // feature to pre-render some pages to html
+    webpack: function (config) {
+      config.plugins = config.plugins || [];
+
+      config.plugins = [
+        ...config.plugins,
+
+        // Read the .env file
+        new Dotenv({
+          path: path.join(__dirname, ".env"),
+          systemvars: true,
+        }),
+      ];
+
+      return config;
     },
   })
 );
