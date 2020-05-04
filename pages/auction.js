a356737e788584f7b39c20edf998a9a4c010102f
Foxbuka <krushenitskii@gmail.com>
2020-05-04 14:13:41 +0300

    New auction in a process

--- pages/auction.js
+++ pages/auction.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Descriptions, Avatar, Card, Divider, List, Typography, Statistic, Layout } from 'antd';
-import { PLAYERS } from '../constants/users';
-import Page from '../containers/Page';
+import React, { useState } from "react";
+import {
+  Button,
+  Descriptions,
+  Avatar,
+  Card,
+  Divider,
+  List,
+  Typography,
+  Statistic,
+  Layout,
+} from "antd";
+import { PLAYERS } from "../constants/users";
+import Page from "../containers/Page";
+import "../styles/main.scss";
 
 const { Sider, Content } = Layout;
 const { Text, Title } = Typography;
@@ -16,11 +27,15 @@ const topRaisedPlayer = PLAYERS.reduce(function (prev, current) {
 const InvestorsList = ({ players }) => {
   return (
     <List
-      itemLayout='horizontal'
+      itemLayout="horizontal"
       dataSource={PLAYERS}
       renderItem={(item, idx) => (
         <List.Item>
-          <List.Item.Meta avatar={<Avatar src={item.avatar} />} title={item.title} description={item.amount + '$'} />
+          <List.Item.Meta
+            avatar={<Avatar src={item.avatar} />}
+            title={item.title}
+            description={item.amount + "$"}
+          />
         </List.Item>
       )}
     />
@@ -29,7 +44,6 @@ const InvestorsList = ({ players }) => {
 
 const Auction = ({ ...props }) => {
   const [topPlayer, setTopPlayer] = useState(topRaisedPlayer);
-  // TODO: Raise bet
 
   const onRaiseBet = () => {
     const amount = topPlayer.amount + 1000;
@@ -41,48 +55,26 @@ const Auction = ({ ...props }) => {
 
   return (
     <Page>
-      <Layout className='bg-white'>
-        <Sider id='sidebar'>
-          <Card title='Инвесторы' className='border-bottom-0 h-100 rounded-0'>
+      <Layout className="bg-white">
+        <Sider id="sidebar">
+          <Card title="Инвесторы" className="border-bottom-0 h-100 rounded-0">
             <InvestorsList players={PLAYERS} />
           </Card>
         </Sider>
-        <Content className='container-fluid  shape-background'>
-          <div className='mt-4 d-flex flex-column justify-content-center align-items-center'>
-            <Title>ТОП ИНВЕСТОР</Title>
-            <Avatar src={topPlayer.avatar} size={256} />
-            <div className='mt-4 text-center'>
-              <Title level={4}>{topPlayer.title}</Title>
-              <h6>{topPlayer.rating}</h6>
+        <Content className="container">
+          <div class="row bets__card">
+            <div class="col bets__profile">
+              <Avatar src={topPlayer.avatar} size={156} />
+              <h3>Дон Хакатон</h3>
+              <p>CEO</p>
             </div>
+            <div class="col">
+              <h3>StartupBakery</h3>
+              <p>Описание проекта</p>
+              <p>Кто делает проект</p>
+              <p>Счетчки после старта реализации</p>
 
-            <Divider className='bg-light' />
-
-            <Descriptions size='medium' column={1}>
-              <Descriptions.Item label='Максимальная инвестиция'>
-                <span className='font-weight-bold'>{topPlayer.amount}$</span>
-              </Descriptions.Item>
-              <Descriptions.Item label='Таймер'>
-                <small>
-                  <Countdown
-                    valueRender={(element) => {
-                      return (
-                        <div>
-                          <span className='font-weight-bold'>{element}</span>
-                        </div>
-                      );
-                    }}
-                    valueStyle={{ fontSize: 'small' }}
-                    value={TIMER_DEADLINE}
-                    format='D дней H часов m минут s'
-                  />
-                </small>
-              </Descriptions.Item>
-            </Descriptions>
-            <div className='my-4 mx-4 w-100 d-flex justify-content-start'>
-              <Button onClick={onRaiseBet} type='primary' shape='round' className='float-right btn btn-primary' ghost>
-                <b>Поднять</b>
-              </Button>
+              <a className="btn btn-primary mb-3 float-right">Поднять ставку</a>
             </div>
           </div>
         </Content>
