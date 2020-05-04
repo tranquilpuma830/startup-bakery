80bb9b400c2eb421e604aec8bd1f1e32d8c79941
DanilR <aringai09@gmail.com>
2020-05-04 10:40:15 +0300

    Remove login form

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <Layout className='cover-container d-flex h-100 mx-auto flex-column bg-white bg-half-filled'>
       <header className='masthead mb-auto'>
-        <div className='inner bg-white p-3'>
+        <div className='inner bg-white p-3 mb-2'>
           <nav className='d-flex nav nav-masthead justify-content-between align-items-center font-weight-bold'>
             <a className='navbar-brand' href='#'>
               <img src='/logo.svg' width='60' height='60' className='d-inline-block' alt='Startup bakery' />
@@ -41,24 +41,15 @@ export default function Home() {
         </div>
       </header>
       <Content className='d-flex p-4 ml-2 align-items-center bg-guy'>
-        <div className='my-auto h-100'>
-          <Form {...layout} name='basic' initialValues={{ remember: true }} className='my-auto'>
-            <Title>Войти</Title>
+        <div className='my-auto w-25'>
+          <Title className=''>Lorem ipsum</Title>
 
-            <Form.Item name='username' rules={[{ required: true, message: 'Пожалуйста, введите email' }]}>
-              <Input placeholder='Email' />
-            </Form.Item>
-
-            <Form.Item name='password' rules={[{ required: true, message: 'Пожалуйста, введите пароль' }]}>
-              <Input.Password placeholder='Пароль' />
-            </Form.Item>
-
-            <Form.Item>
-              <Link href='ideas'>
-                <a className='btn btn-outline-primary'>Дальше</a>
-              </Link>
-            </Form.Item>
-          </Form>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac risus enim. Proin eu ornare quam. Proin et
+            dui quam. Vivamus in malesuada nisl, vitae fermentum ligula. Vestibulum mollis ante tincidunt, facilisis
+            risus et, pretium felis. Etiam ut est id risus tincidunt facilisis. Vestibulum viverra libero suscipit nisi
+            tempus porta. Quisque ullamcorper in tortor id feugiat. Aliquam facilisis eu leo nec fermentum.
+          </Text>
         </div>
       </Content>
     </Layout>
