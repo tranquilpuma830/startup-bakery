c0efd4dc791560b66b6d89a31db05039cef838b3
Malakhov Dmitriy <dmitriy.malakhov@gorparkovki.ru>
2020-05-04 20:39:13 +0300

    changes

--- constants/projects.js
+++ constants/projects.js
@@ -1,32 +1,37 @@
 const PROJECT_OVERVEIW = [
   {
-    type: "text",
-    content: `
-          Three years ago, we launched our first product on Kickstarter:
-          an expressive and modular MIDI controller for seasoned
-          musicians, which is used today by many artists around the
-          world.`,
-  },
-  {
-    type: "text",
-    content: `
-        But lots of amateur musicians wanted to get their hands on it
-        as well, and we felt frustrated that we couldn’t satisfy them,
-        as current music software remains too complex to be
-        apprehended easily. So we decided to create a new instrument
-        that would enable anyone to start making music.`,
-  },
-  {
-    type: "text",
-    content: `
-        An instrument that’s really easy to use and fun, while being
-        sufficiently powerful and evolutive to take you far into your
-        music creation. Introducing the Joué Play.`,
-  },
-  {
-    type: "image",
+    type: "title",
     content:
-      "https://ksr-ugc.imgix.net/assets/028/865/455/3e068f371806f85bde338d0f9aa7a439_original.jpg?ixlib=rb-2.1.0&w=680&fit=max&v=1587973373&auto=format&frame=1&q=92&s=cc2736db205c26d86ce8120ff31210e9",
+      "Что такое STARTUP BAKERY?",
+  },
+  {
+    type: "text",
+    content: `Уникальная площадка, где каждый может разместить свою проектную идею и довести ее результата.`,
+  },
+  {
+    type: "text",
+    content: `
+    Присоединяйся к Startup Bakery, и с нашей помощью ты сможешь “выпечь” свой стартап, пройдя самые трудные этапы, на которых чаще всего перегорают проекты.`,
+  },
+  {
+    type: "title",
+    content: `99% стартапов умирают в первые полгода своего существования`,
+  },
+  {
+    type: "text",
+    content: `Ключевые минусы стартапов - мало опыта и много амбиций команды.`,
+  },
+  {
+    type: "text",
+    content: `В большинстве случаев команда создает продукт, который не нужен рынку, перегорает или не может найти партнера для реализации своей идеи.`,
+  },
+  {
+    type: "title",
+    content: `Аукцион`,
+  },
+  {
+    type: "text",
+    content: ` Самые «горячие» стартапы принимают участие в аукционе, где любой инвестор может предложить свое финансирование проекта.`,
   },
 ];
 
@@ -83,11 +88,12 @@ MPE compatible, the Play Pads have fixed settings.`,
 ];
 
 export const PROJECT_DATA = {
-  title: "Joué Play - Everyone can play music",
+  title: "Startup Bakery",
   subtitle:
-    "make a beat, play a melody and build chords naturally on a digital yet expressive music instrument.",
+    `Startup Bakery - уникальная площадка, где каждый может разместить свою проектную идею и довести ее результата.
+    Присоединяйся к Startup Bakery, и с нашей помощью ты сможешь “выпечь” свой стартап, пройдя самые трудные этапы, на которых чаще всего перегорают проекты.`,
   cover_img:
-    "https://ksr-ugc.imgix.net/assets/028/891/063/7728109c7056da046134bed4de4ca578_original.jpg?ixlib=rb-2.1.0&w=680&fit=max&v=1588138264&auto=format&frame=1&q=92&s=11ed4fa59a43dc1da233e5b706e8ff24",
+    "",
   overview: PROJECT_OVERVEIW,
   comments: COMMENTS,
   FAQ,
